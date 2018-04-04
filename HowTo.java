7fbb668a53c2060fbbe310428c600e198f7b86ff
rachelgelmis <37519873+rachelgelmis@users.noreply.github.com>
2018-04-03 21:53:54 -0500

    Add files via upload

---
+++ HowTo.java
@@ -0,0 +1,27 @@
+
+public class HowTo
+{
+	private String rouletteInstructions = "TO PLAY ROULETTE:";
+	private String crapsInstructions = "TO PLAY CRAPS: ";
+	private String blackjackInstructions = "TO PLAY BLACKJACK: ";
+	
+	private HowTo()
+	{
+		
+	}
+	
+	public void printRouletteInstructions()
+	{
+		System.out.println(rouletteInstructions);
+	}
+	public void printCrapsInstructions()
+	{
+		System.out.println(crapsInstructions);
+	}
+	public void printBlackjackInstructions()
+	{
+		System.out.println(blackjackInstructions);
+	}
+	
+
+}
