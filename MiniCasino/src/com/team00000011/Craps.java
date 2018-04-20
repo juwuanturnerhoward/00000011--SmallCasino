1c4d206b0b51622cd48c489c58131eb094d9d7f1
Kaleb Nelson <kalebnn@gmail.com>
2018-04-19 19:33:19 -0500

    GUI Reworks
    Added functions related to gui use
    Reworked some logic 
    Die checks no longer return bools
       Needed for GUI use

--- MiniCasino/src/com/team00000011/Craps.java
+++ MiniCasino/src/com/team00000011/Craps.java
@@ -1,7 +1,12 @@
-package com.team00000011;
+package test1;
 
-import com.team00000011.Die;
-import com.team00000011.GamePlayers.CrapsPlayer;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -9,74 +14,104 @@ import com.team00000011.GamePlayers.CrapsPlayer;
  */
 public class Craps extends Game
 {
+  
+  
   @Override
   public void play()
   {
-    System.out.println("Welcome to Craps! The simple dice rolling game!");
-    System.out.println("Please press R to roll the die!"); 
+    
   }
   
   @Override
   public void returnHome() {}
   
-  private boolean checkFirstRoll()
+  /**
+   * Checks the first roll of the game; Uses a tracker variable inside the game
+   * logic to check if needed
+   */
+  void checkFirstRoll()
   {
+    //Gets the total value of the dice roll
+    getDiceTotalValue(Dice[0],Dice[1]);
+    
+    //If/else block for checking of die
+    //Checks if the user rolled 7 or 11, which is a win
     if(this.currentDiceSum == 7 || this.currentDiceSum== 11)
     {
-      System.out.println("Congrats! You won your bet!");
-      return true;
+      popupWindow("Congrats! You rolled a " + 
+              Integer.toString(this.currentDiceSum) +"! You won" +
+              Integer.toString(gamePlayer.getCurrentBet() * 2) +" dollars!");
+      gamePlayer.user.addFunds(gamePlayer.getCurrentBet() * 2);
+      gamePlayer.placeBet(0);
+      Dice[0].setValue(0);
+      Dice[1].setValue(0);
+      this.targetDiceValue = 0;
     }
+    
+    //Checks if the user rolled a 2,3 or 12, which is an automatic loss
     else if(this.currentDiceSum == 2 || this.currentDiceSum == 3 || 
             this.currentDiceSum == 12)
     {
-      System.out.println("Sorry! You rolled a " + this.currentDiceSum + 
-              "! You lost your bet");
-      return true;
+      popupWindow("Sorry! You rolled a " + 
+              Integer.toString(this.currentDiceSum) + "! You lost your bet...");
+      gamePlayer.placeBet(0);
+      Dice[0].setValue(0);
+      Dice[1].setValue(0);
+      this.targetDiceValue = 0;
     }
+    
+    //Continues informs user to roll again
     else
     {
-      System.out.println("You haven't won yet! Roll again for another chance "
-              + "to win!");
+      popupWindow("You haven't won yet! Roll again for another chance to win!");
       this.targetDiceValue = this.currentDiceSum;
-      return false;
     }
   }
   
   /**
-   * This function 
-   * @return true if compareDiceValues returns true OR if currentDiceSum is
-   * equal to 2, 3, or 12
+   * This function checks the next roll of the die, being implemented with a 
+   * tracking variable inside of the game logic
    */
-  private boolean checkNextRoll()
+  void checkNextRoll()
   {
+    getDiceTotalValue(Dice[0],Dice[1]);
+    
     if(compareDiceValues() == true)
     {
     
-      System.out.println("Congrats! You hit the target dice value!"
-              + "You won " + (currentBet * 2) + "dollars!");
-      return true;
+      popupWindow("Congrats! You hit the target dice value!"
+              + "You won " + Integer.toString(gamePlayer.getCurrentBet() * 2) + 
+              "dollars!");
+      
+      gamePlayer.user.addFunds(gamePlayer.getCurrentBet() * 2);
+      gamePlayer.placeBet(0);
+      Dice[0].setValue(0);
+      Dice[1].setValue(0);
+      this.targetDiceValue = 0;
     }
     else if (this.currentDiceSum == 2 || this.currentDiceSum == 3 || 
-              this.currentDiceSum == 12)
+            this.currentDiceSum == 7 || this.currentDiceSum == 12)
     {
-      System.out.println("Sorry! You rolled a " + this.currentDiceSum + 
-              "! You lost your bet");
-      return true;
+      popupWindow("Sorry! You rolled a " + Integer.toString(this.currentDiceSum)
+              + "! You lost your bet");
+      gamePlayer.placeBet(0);
+      Dice[0].setValue(0);
+      Dice[1].setValue(0);
+      this.targetDiceValue = 0;
     }
+    
     else
     {
-      System.out.println("You haven't hit your target value yet! Roll again!");
-      return false;
+      popupWindow("You haven't hit your target value yet! Roll again!");
     }
   }
-  
   /**
    * This function serves to check if currentDiceSum and targetDiceValue are 
    * are equal to one another within checkNextRoll()
    * @return true if the currentDiceSum and targetDiceValue are equal
    * @return false if currentDiceSum and targetDiceValue are not equal
    */
-  private boolean compareDiceValues()
+  boolean compareDiceValues()
   {
     if(this.currentDiceSum == this.targetDiceValue)
     {
@@ -88,7 +123,6 @@ public class Craps extends Game
       return false;
     }
   }
-  
   /**
    * This function serves to add together the two values of the die objects
    * to give an integer for use in later functions.
@@ -104,9 +138,39 @@ public class Craps extends Game
     this.currentDiceSum = dieSum;
   }
   
-  private int currentBet;
-  private Die[] Dice = new Die[2];
-  private CrapsPlayer gamePlayer = new CrapsPlayer();
-  private int targetDiceValue;
+  /**
+   * This function will create a popup window with the text of input in the
+   * window
+   * @param input a string to be printed in the popup window
+   */
+  private void popupWindow(String input)
+  {
+    Stage popup = new Stage();
+      
+      popup.initModality(Modality.APPLICATION_MODAL);
+      
+      Label message = new Label(input);
+     
+      Button close = new Button("Close");
+      close.setOnAction(e->popup.close());
+      
+      VBox layout= new VBox(10);
+     
+      
+      layout.getChildren().addAll(message, close);
+      
+      layout.setAlignment(Pos.CENTER);
+      
+      Scene scene1= new Scene(layout, 300, 250);
+      
+      popup.setScene(scene1);
+      
+      popup.showAndWait();
+  }
+  
+  //Variables for use in class
+  public Die[] Dice = new Die[2];
+  public CrapsPlayer gamePlayer = new CrapsPlayer("Johnny");
+  public int targetDiceValue;
   private int currentDiceSum;
 }
